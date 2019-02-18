64a707bcb19d1036675fbea03c036154d63b5596
Somenath <somenathmondal@avataar.me>
2019-02-18 12:39:09 +0530

    Initial Commit

---
+++ CheckBodyImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckBodyImages
+//
+//  Created by Somenath on 18/02/19.
+//  Copyright © 2019 Somenath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
